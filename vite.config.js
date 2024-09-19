cdb7e49b1b340910f0e07bedf7b4ccdfdc416bb9
Kaviyarasu P <kaviyarasu2224@gmail.com>
2024-09-19 10:35:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Website-Header-design/',  // Adjust this if the repository name changes
+  base: '/Website-Header-Design/',  // Adjust this if the repository name changes
 });
